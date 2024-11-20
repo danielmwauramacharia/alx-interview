175838f304d2f27200ed9468444ebee3621edf62
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-11-20 06:48:12 +0300

    adding a code of different way to solve the the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,31 +14,45 @@ def pascal_triangle(n):
         n (Integer): The integer should be positive and > than zero.
     """
 
+    # # This was my solution
+    # if not isinstance(n, int):
+    #     raise TypeError("n should be an integer")
+    # mainlist = []
+    # if n <= 0:
+    #     return mainlist
+    # if n == 1:
+    #     mainlist.append([1])
+    #     return mainlist
+    # if n > 2:
+    #     mainlist.append([1])
+    #     mainlist.append([1, 1])
+    # if n == 2:
+    #     return mainlist
+    # x = 3
+    # while x <= n:
+    #     new = []
+    #     val = x - 2
+    #     wrklist = mainlist[val]
+    #     for i in range(val):
+    #         val1 = wrklist[i]
+    #         val2 = wrklist[i + 1]
+    #         value = val1 + val2
+    #         new.append(value)
+    #     new.insert(0, 1)
+    #     new.append(1)
+    #     x += 1
+    #     mainlist.append(new)
+    # return mainlist
+
+    # below is the solution after research
     if not isinstance(n, int):
-        raise TypeError("n should be an integer")
-    mainlist = []
+        raise TypeError("n must be an integer")
     if n <= 0:
-        return mainlist
-    if n == 1:
-        mainlist.append([1])
-        return mainlist
-    if n > 2:
-        mainlist.append([1])
-        mainlist.append([1, 1])
-    if n == 2:
-        return mainlist
-    x = 3
-    while x <= n:
-        new = []
-        val = x - 2
-        wrklist = mainlist[val]
-        for i in range(val):
-            val1 = wrklist[i]
-            val2 = wrklist[i + 1]
-            value = val1 + val2
-            new.append(value)
-        new.insert(0, 1)
-        new.append(1)
-        x += 1
-        mainlist.append(new)
-    return mainlist
+        return []
+    triangle = [[1]]
+    for _ in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1] + [prev_row[j] + prev_row[j + 1]
+                         for j in range(len(prev_row) - 1)] + [1]
+        triangle.append(new_row)
+    return triangle
